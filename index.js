bb7d9c5bcd827b5ea37d5d998171abedfbeedd1a
soni895 <darshansoni895@gmail.com>
2023-07-22 23:49:53 +0530

    db connect

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express=require("express");
 const app=express();
 require('dotenv').config();
+app.use(express.json());
+const {db_connect}=require("./config/database")
 const port=process.env.port||4000
 
 app.listen(port,()=>
 {
     console.log(`hi darshan soni`);
+});
+db_connect();
 
+// default route
+app.get("/",(req,res)=>
+
+{
+    res.send("<h1>Hi darshan soni</h1>");
 })
