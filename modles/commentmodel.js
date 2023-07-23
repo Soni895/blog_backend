4e9799492575ca8defc552cdd10deba5e1700fcd
soni895 <darshansoni895@gmail.com>
2023-07-23 22:42:03 +0530

    comment schema completed

---
+++ modles/commentmodel.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const commentschema=new mongoose.Schema(
+    {
+
+post:
+{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"post", // refereence to the post model
+},
+user:
+{
+    type:String,
+    require:true,
+},
+body:{
+    type:String,
+    required:true,
+}
+    }
+);
+
+module.exports=mongoose.model("comment",commentschema);
