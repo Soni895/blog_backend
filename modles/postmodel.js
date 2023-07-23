c76756fe56576e4709fc7ba4c37dae7df8f81694
soni895 <darshansoni895@gmail.com>
2023-07-23 22:58:12 +0530

    models completed

---
+++ modles/postmodel.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+const postschema= new mongoose.Schema({
+
+title:
+{
+    type:String,
+    require:true,
+},
+body:
+{
+    type:String,
+    require:true,
+},
+like:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"like",
+
+}]
+,
+comemnt:
+[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"comment",
+}]
+
+
+    
+});
+module.exports=mongoose.model("post",postschema);
