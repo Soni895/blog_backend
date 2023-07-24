f266708cd1e32001ff32860e3de8f0cadd3c8165
soni895 <darshansoni895@gmail.com>
2023-07-24 21:55:59 +0530

    code test successfull

--- controllers/createcomment.js
+++ controllers/createcomment.js
@@ -1,4 +1,4 @@
-const comment=require("../modles/commentmodel");
+const Comment=require("../modles/commentmodel");
 const Post=require("../modles/postmodel");
 
 exports.createcomment=async (req,res)=>
@@ -7,21 +7,21 @@ exports.createcomment=async (req,res)=>
         // fatch data
         const {post,user,body}=req.body;
         // craete comment
-        const Comment= new comment(
+        const comment= new Comment(
             { 
                 post,user,body
             }
         );
         // save object into data base
-        const savecomment= await Comment.save();
+        const savecomment= await comment.save();
 
         //find the id of new comment and save it to the post comment  array
 
        const updatedpost = await Post.findByIdAndUpdate(post,
-        {$push:{comment:savecomment.id}},
-        {new :true})
+        {$push:{comment: savecomment._id}},
+        {new: true})
         .populate("comment")
-        .exce();
+        .exec();
 
         res.json(
             {
@@ -32,13 +32,11 @@ exports.createcomment=async (req,res)=>
         
     } 
     catch (error) {
-        res.status(500).json(
+       res.status(500).json(
             {
-                error:"comment not save"
+                error:"comment not save",
             }
         );
-
-
-
     }
 }
+
