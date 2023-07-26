9fd88f650a5d39484b35856f210aaa95913a6c63
soni895 <darshansoni895@gmail.com>
2023-07-26 20:09:15 +0530

    data name change

--- controllers/createcomment.js
+++ controllers/createcomment.js
@@ -24,7 +24,7 @@ exports.createcomment=async(req,res)=>
         res.status(200).json(
             {
                 response:response,
-                update_data:update_data, 
+                update_post:update_data, 
                 status :"comment saved successfull",
             }
         )
