c4abf05f9342f5af7bde17f0b92d47df90f3cc0a
soni895 <darshansoni895@gmail.com>
2023-07-27 11:19:19 +0530

    changes apply comment test successfull

--- controllers/createcomment.js
+++ controllers/createcomment.js
@@ -1,18 +1,18 @@
-const post=require("../modles/postmodel");
-const comment=require("../modles/commentmodel");
+const Post=require("../modles/postmodel");
+const Comment=require("../modles/commentmodel");
 
 exports.createcomment=async(req,res)=>
 {
 
     try{
-        const {user,body,post:Post}=req.body;
-        const createcomment= new comment(
+        const {user,body,post}=req.body;
+        const createcomment= new Comment(
             {
-                Post,user,body
+                post,user,body
             }
         );
         const response= await createcomment.save();
-        const update_post=await post.findByIdAndUpdate(Post,
+        const update_post=await Post.findByIdAndUpdate(post,
             {$push:{comment:response._id}},
             {new: true})
             .populate("comment")
@@ -23,7 +23,7 @@ exports.createcomment=async(req,res)=>
         res.status(200).json(
             {
                 response:response,
-                update_post:update_data, 
+                update_post:update_post, 
                 status :"comment saved successfull",
             }
         )
